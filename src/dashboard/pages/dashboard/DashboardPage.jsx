dcc2d66c54db1494cb10f5b80593e7ec1e6ef9f5
Josue Rocha <josue.rocha0809@gmail.com>
2023-08-31 18:35:27 -0600

    try notifications

--- src/dashboard/pages/dashboard/DashboardPage.jsx
+++ src/dashboard/pages/dashboard/DashboardPage.jsx
@@ -14,12 +14,18 @@ import ArticleIcon from '@mui/icons-material/Article';
 
 import './chatsStyles.css';
 import ChatView from '../chat/ChatView';
+import Pusher from 'pusher-js';
+import { toast } from 'react-toastify';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
 export const DashboardPage = () => {
   const { t } = useTranslation();
   const [data, setData] = useState([]);
   const [responseUser, setResponseUser] = useState([]);
   const [dataItems, setDataItems] = useState([]);
+  const [notificationBody, setNotificationBody] = useState([]);
+  const [notificationContact, setNotificationContact] = useState([]);
   const { isLightTheme } = useSelector((state) => state.ui);
   const language = localStorage.getItem('i18nextLng');
   const { loading } = useSelector((state) => state.dashboard);
@@ -31,6 +37,35 @@ export const DashboardPage = () => {
     setData(arr);
     setResponseUser(resp?.data.pending_items);
   };
+
+  let allMessages = [];
+  useEffect(() => {
+    console.log('aqui ssssss');
+    //Pusher.logToConsole = true;
+    const pusher = new Pusher('87a001442582afe960c1', { cluster: 'us2' });
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', function (data) {
+      allMessages.push(data);
+      const jsonObject = JSON.parse(data.message);
+      console.log('aqui oaou', jsonObject);
+      jsonObject.thread.contact;
+      jsonObject.body;
+      setNotificationBody(jsonObject.body);
+      setNotificationContact(jsonObject.thread.contact);
+      toast.error(`${jsonObject.thread.name}:${jsonObject.body}`,{
+        autoClose: false
+      })
+    });
+  }, []);
+  // useEffect(() => {
+  //   Pusher.logToConsole = true;
+  //   const pusher = new Pusher('a6f0142583a796bae3a3', { cluster: 'us2' });
+  //   const channel = pusher.subscribe('chat');
+  //   channel.bind('message', function (data) {
+  //     allMessages.push(data);
+  //     setMessages(allMessages);
+  //   });
+  // });
 
   useEffect(() => {
     const pendingItems = responseUser?.map((item) => ({
@@ -48,12 +83,12 @@ export const DashboardPage = () => {
   return (
     <DashboardLayout>
       {loading && <Loader />}
-      <Grid className={'container'}  container>
+      <Grid className={'container'} container>
         <Grid container>
           <Grid item xs={12}>
-      <Card sx={{textAlign:'center', width:'100%', height:'100%'}} >
-      <img src='/images/vivechat.png' width='15%' alt='' style={{marginTop:'20%'}} />
-      </Card>
+            <Card sx={{ textAlign: 'center', width: '100%', height: '100%' }}>
+              <img src='/images/vivechat.png' width='15%' alt='' style={{ marginTop: '20%' }} />
+            </Card>
           </Grid>
         </Grid>
       </Grid>
