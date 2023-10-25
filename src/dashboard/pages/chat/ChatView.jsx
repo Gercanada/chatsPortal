1d2af72b5df1b1aac7c2a7cda0d9f8911240ecb3
Josue Rocha <josue.rocha0809@gmail.com>
2023-10-24 18:26:58 -0600

    dev: audios recorder in building

--- src/dashboard/pages/chat/ChatView.jsx
+++ src/dashboard/pages/chat/ChatView.jsx
@@ -18,10 +18,9 @@ import Pusher from 'pusher-js';
 import { useTranslation } from 'react-i18next';
 
 const ChatView = () => {
-
   // const [messagesReponseState , setMessageResponseState] = useState(onechat?.data)
   const { onechat } = useSelector((state) => state.whatsApp);
-//const [messagesReponseState , setMessageResponseState] = useState(onechat?.data)
+  //const [messagesReponseState , setMessageResponseState] = useState(onechat?.data)
 
   const [sortMessages, setSortMessages] = useState([]);
   const [pageNumber, setPageNumber1] = useState(1);
@@ -32,82 +31,83 @@ const ChatView = () => {
   const [hasChange, setHasChange] = useState(false);
   const { t } = useTranslation();
   const [data, setData] = useState([]);
-  const [newMessage, setNewMessage] = useState('')
+  const [newMessage, setNewMessage] = useState('');
   const [responseUser, setResponseUser] = useState([]);
   const [dataItems, setDataItems] = useState([]);
   const [notificationBody, setNotificationBody] = useState([]);
   const [notificationContact, setNotificationContact] = useState([]);
   const language = localStorage.getItem('i18nextLng');
-  
 
   const playSound = () => {
     //const audioElement = new Audio('/public/images/whistle-campana-whatsapp.mp3');
- //  audioElement.play();
- };
+    //  audioElement.play();
+  };
   const allMessages = [];
 
- useEffect(() => {
-
-   //Pusher.logToConsole = true;
-   const pusher = new Pusher('87a001442582afe960c1', { cluster: 'us2' });
-   const channel = pusher.subscribe('chat');
-   channel.bind('message', function (data) {
-    // playSound();
-     allMessages.push(data);
-     const jsonObject = JSON.parse(data.message);
-     console.log('jsonObject',jsonObject)
-     jsonObject.thread.contact;
-     jsonObject.body;
-     setNotificationBody(jsonObject.body);
-     setNotificationContact(jsonObject.thread.contact);
-    //  toast.error(`${jsonObject.thread.name}:${jsonObject.body}`,{
-    //    autoClose: false
-    //  })
-      toast.error(t(`new_message`),{
-       autoClose: false
-     })
-     loadChats()
-   });
- }, []);
+  useEffect(() => {
+    //Pusher.logToConsole = true;
+    const pusher = new Pusher('87a001442582afe960c1', { cluster: 'us2' });
+    const channel = pusher.subscribe('chat');
+    channel.bind('message', function (data) {
+      // playSound();
+      allMessages.push(data);
+      const jsonObject = JSON.parse(data.message);
+      console.log('jsonObject', jsonObject);
+      jsonObject.thread.contact;
+      jsonObject.body;
+      setNotificationBody(jsonObject.body);
+      setNotificationContact(jsonObject.thread.contact);
+      //  toast.error(`${jsonObject.thread.name}:${jsonObject.body}`,{
+      //    autoClose: false
+      //  })
+      toast.error(t(`new_message`), {
+        autoClose: false,
+      });
+      loadChats();
+    });
+  }, []);
 
   const loadChats = async () => {
     const resp = await dispatch(getUserChat(id));
     if (resp) {
-      const reversedArray = sortArray(resp?.data?.data?.data)
-      console.log('reverrrrr',reversedArray)
-      const markAsRead = reversedArray?.map((item)=> ( item.id))
-      console.log('mard as read', markAsRead)
-      dispatch(setReadMessages(markAsRead))
+      const reversedArray = sortArray(resp?.data?.data?.data);
+      console.log('reverrrrr', reversedArray);
+      const markAsRead = reversedArray
+        .filter((item) => item.Contact === item.from) 
+        .map((item) => item.id); 
+
+      console.log('mard as read', markAsRead);
+      dispatch(setReadMessages(markAsRead));
       setSortMessages(reversedArray);
       setHasMoreChats(resp?.data?.data?.last_page);
     }
   };
   useEffect(() => {
     loadChats();
-  }, [id,hasChange]);
+  }, [id, hasChange]);
 
   const sortArray = (arrayResponse) => {
-    const array = arrayResponse
+    const array = arrayResponse;
     const copyArray = [...array];
     const reversedArray = copyArray.reverse();
     return reversedArray;
-  }
+  };
 
   const loadMoreChats = async () => {
-    let page = 0
-    if(pageNumber === 0){
+    let page = 0;
+    if (pageNumber === 0) {
       page = pageNumber + 2;
-    }else{
+    } else {
       page = pageNumber + 1;
     }
-   // const pageNumberCounter = pageNumber + 1;
+    // const pageNumberCounter = pageNumber + 1;
     setPageNumber1(page);
     const response = await dispatch(getMoreMessages(thread, page));
     if (response && response?.data) {
-      const reversedArray = sortArray(response?.data?.data?.data)
-      const markAsRead = reversedArray?.map((item)=> ( item.id))
-      console.log('mard as read', markAsRead)
-      dispatch(setReadMessages(markAsRead))
+      const reversedArray = sortArray(response?.data?.data?.data);
+      const markAsRead = reversedArray?.map((item) => item.id);
+      console.log('mard as read', markAsRead);
+      dispatch(setReadMessages(markAsRead));
       setSortMessages((prevChats) => [...reversedArray, ...prevChats]);
       setHasMoreChats(response?.data?.data?.last_page);
     } else {
@@ -117,7 +117,7 @@ const ChatView = () => {
 
   useEffect(() => {
     dispatch(getUserChat(id));
-  }, [id,thread,hasChange]);
+  }, [id, thread, hasChange]);
 
   return (
     <DashboardLayout>
@@ -135,9 +135,13 @@ const ChatView = () => {
           />
         </Grid>
         <Grid item xs={12}>
-        <MessagesField  setHasChange={setHasChange} loadChats={loadChats} setNewMessage={setNewMessage} />
+          <MessagesField
+            setHasChange={setHasChange}
+            loadChats={loadChats}
+            setNewMessage={setNewMessage}
+          />
+        </Grid>
       </Grid>
-    </Grid>
     </DashboardLayout>
   );
 };
