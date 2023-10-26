be34139ca6204404b302064f3ddc80e9b26158af
Josue Rocha <josue.rocha0809@gmail.com>
2023-10-25 18:34:42 -0600

    dev: preview of images and docs

--- src/components/Modal/PreviewModal.jsx
+++ src/components/Modal/PreviewModal.jsx
@@ -1,59 +1,69 @@
-import {
-    Box,
-    Modal,
-  } from '@mui/material';
-  import React from 'react';
-  
-  const modalStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+import { Box, Modal } from '@mui/material';
+import React, { useState } from 'react';
+
+const modalStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const contentStyle = {
+  backgroundColor: 'white',
+  border: '2px solid #000',
+  borderRadius: '4px',
+  padding: '20px',
+  width: '70%', // Ancho fijo de 300 píxeles
+  height: '60%', // Alto fijo de 300 píxeles
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const PreviewModal = ({ open, onClose, attachmentFile }) => {
+  const [numPages, setNumPages] = useState(null);
+
+  const handleClose = () => {
+    onClose(false);
   };
-  
-  const contentStyle = {
-    backgroundColor: 'white',
-    border: '2px solid #000',
-    borderRadius: '4px',
-    padding: '20px',
-    width: '300px',  // Ancho fijo de 300 píxeles
-    height: '300px', // Alto fijo de 300 píxeles
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
-  
-  const imgStyle = {
-    maxWidth: '100%',
-    maxHeight: '100%',
-    width: 'auto',
-    height: 'auto',
-  };
-  
-  const PreviewModal = ({
-    open,
-    onClose,
-    attachmentFile,
-  }) => {
-    const handleClose = () => {
-      onClose(false);
-    };
-  
-    return (
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
-        sx={modalStyle}
-      >
-        <Box sx={contentStyle}>
-          {attachmentFile && (
-            <img src={URL.createObjectURL(attachmentFile)} style={imgStyle} alt="Preview" />
-          )}
-        </Box>
-      </Modal>
-    );
-  };
-  
-  export default PreviewModal;
-  
+
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby='modal-modal-title'
+      aria-describedby='modal-modal-description'
+      sx={modalStyle}
+    >
+      <Box sx={contentStyle}>
+        {attachmentFile &&
+          (attachmentFile.type.startsWith('image/') ? ( // Comprueba si es una imagen
+            <img
+              src={URL.createObjectURL(attachmentFile)}
+              alt='Preview'
+              style={{
+                maxWidth: '100%',
+                maxHeight: '100%',
+                width: 'auto',
+                height: 'auto',
+              }}
+            />
+          ) : attachmentFile.type === 'application/pdf' ? ( // Comprueba si es un PDF
+          <img
+              src={'../../../public/images/file.svg'}
+              alt='Preview'
+              style={{
+                maxWidth: '100%',
+                maxHeight: '100%',
+                width: 'auto',
+                height: 'auto',
+              }}
+            />
+          ) : (
+            <p>Formato de archivo no compatible.</p>
+          ))}
+      </Box>
+    </Modal>
+  );
+};
+
+export default PreviewModal;
